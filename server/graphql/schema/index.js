bef75fc3006d311c2eafb89c7ef51a1a061aecab
Kiur Jullis <kiur.jullis@gmail.com>
2022-06-04 16:24:32 +0300

    resolver use jwt and bcrypt to authenticate user

--- server/graphql/schema/index.js
+++ server/graphql/schema/index.js
@@ -1,20 +1,22 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-        type RootQuery {
-          users: [User]
-}
         type User {
             id: ID!
             name: String!
             email: String!
             password: String!
-}
-        type RootMutation {
-        createUser(name: String!, email: String!, password: String!): User
-}
+        }
+        type AuthData {
+            userId: ID!
+            token: String!
+            tokenExpiration: Int!
+        } 
+          type RootQuery {
+          users: [User]
+          login(email: String!, password: String!): AuthData//resolver use jwt and bcrypt to authenticate user and create token for user to use in graphql 
+        }
         schema {
         query: RootQuery
-            mutation: RootMutation
-}
+        }
 `);
