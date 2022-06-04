15306b4855496544255fbfeb73440f422e19e482
Kiur Jullis <kiur.jullis@gmail.com>
2022-06-04 16:24:07 +0300

    resolver use jwt and bcrypt to authenticate user

--- server/graphql/resolvers/auth.js
+++ server/graphql/resolvers/auth.js
@@ -4,24 +4,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../model/user');
 
-module.exports = {
-  createUser: async (args) => {
-    try {
-      const existingUser = await User.findOne({ email: args.userInput.email });
-      if (existingUser) {
-        throw new Error('User exists already.');
-      }
-      const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
-      const user = new User({
-        email: args.userInput.email,
-        password: hashedPassword
-      });
-      const result = await user.save();
-      return { ...result._doc, password: null, _id: result.id };
-    } catch (err) {
-      throw err;
-    }
-  },
+module.exports = {//resolver use jwt and bcrypt to authenticate user and create token for user to use in graphql
   login: async ({ email, password }) => {
     const user = await User.findOne({ email: email });
     if (!user) {
