ab7d2e20732fee5d27336d879fb816c85507aafa
Kiur Jullis <kiur.jullis@gmail.com>
2022-06-07 08:28:16 +0300

    rootValue: use graphQlResolvers,

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const mySql = require('mysql');
 const bodyParser = require('body-parser');
 
+//const bcrypt = require('bcryptjs');
+//const jwt = require('jsonwebtoken');
+//const User = require('./model/user');
 
-const User = require('./model/user');
 const graphQlSchema = require('./graphql/schema/index');
-//const graphQlResolvers = require('./graphql/resolvers/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+const isAuth = require('./middlewere/is-auth');
 const cors = require('cors');
 
 const app = express();
@@ -14,16 +16,16 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(isAuth);
 
 app.use(
   '/graphql',
   graphqlHTTP({
     schema: graphQlSchema,
-    rootValue: User,
-    graphiql: true
+    rootValue: graphQlResolvers,
+    graphiql: true,
   })
 );
-
 
 app.listen(8000, () => {
   console.log('Server started on port 8000');
