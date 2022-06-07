abb9c952b9b1996cb3b51573a84a750f8c8317d2
Kiur Jullis <kiur.jullis@gmail.com>
2022-06-07 08:24:04 +0300

    login

--- server/graphql/schema/index.js
+++ server/graphql/schema/index.js
@@ -1,22 +1,26 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
+
         type User {
-            id: ID!
-            name: String!
-            email: String!
-            password: String!
+                id: ID!
+                name: String
+                email: String!
+                password: String              
         }
+
         type AuthData {
-            userId: ID!
-            token: String!
-            tokenExpiration: Int!
-        } 
-          type RootQuery {
-          users: [User]
-          login(email: String!, password: String!): AuthData//resolver use jwt and bcrypt to authenticate user and create token for user to use in graphql 
+                userId: ID!
+                token: String!
+                tokenExpiration: Int!
         }
+
+        type RootQuery {
+                users: [User!]!
+                login(email: String!, password: String!): AuthData!
+        }
+
         schema {
-        query: RootQuery
+                query: RootQuery
         }
 `);
